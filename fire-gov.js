ca8783340352d7ebcb88031841bfae9da672b31e
Marijn Koolen <marijn.koolen@gmail.com>
2014-06-23 11:11:28 +0200

    first commit

---
+++ fire-gov.js
@@ -0,0 +1,9 @@
+var Firebase = require('firebase');
+var dataUrl = "https://pilotcthdata.firebaseio.com/"
+var testRef = new Firebase(testUrl + "link_check");
+var linkRef = new Firebase(dataUrl + "dataoverheidnl/26-05-14/links");
+var dataRef = new Firebase(dataUrl + "dataoverheidnl/26-05-14/datasets");
+var testRef = new Firebase(dataUrl + "dataoverheidnl/26-05-14/datasets/000b1208-433f-4a12-a60c-f2e092815ec6");
+
+
+

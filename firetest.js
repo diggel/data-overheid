742a29665ab37f3cbb900d77f2ba77484478b56b
Reinier <tromp.rc@gmail.com>
2014-06-26 12:12:09 +0200

    change firetest

--- firetest.js
+++ firetest.js
@@ -1,24 +1,25 @@
 var Firebase = require('firebase');
-var dataUrl = "https://pilotcthdata.firebaseio.com/";
-var fireRef = new Firebase("https://pilotcthdata.firebaseio.com/dataoverheidnl/26-05-14/");
-var datasetRef = new Firebase("https://pilotcthdata.firebaseio.com/dataoverheidnl/26-05-14/datasets");
-var linksRef = new Firebase ("https://pilotcthdata.firebaseio.com/dataoverheidnl/26-05-14/links");
-var resourcesRef = new Firebase("https://pilotcthdata.firebaseio.com/dataoverheidnl/26-05-14/datasets/000b1208-433f-4a12-a60c-f2e092815ec6/resources");
+var dataUrl = "https://cth-data-overheid-rj.firebaseio.com/";
+// var fireRef = new Firebase("https://pilotcthdata.firebaseio.com/dataoverheidnl/26-05-14/");
+var datasetRef = new Firebase("https://cth-data-overheid-rj.firebaseio.com/rows");
+// var linksRef = new Firebase ("https://pilotcthdata.firebaseio.com/dataoverheidnl/26-05-14/links");
+// var resourcesRef = new Firebase("https://pilotcthdata.firebaseio.com/dataoverheidnl/26-05-14/datasets/000b1208-433f-4a12-a60c-f2e092815ec6/resources");
 
 
 
-var query = datasetRef
+var query = datasetRef.limit(10)
 // var query = linksRef
 var urls = [];
-
+var tags = {};
+var result = [];
 
 query.on('value', function(snapshot) {
-  var result = snapshot.val();
-  for (var i in result){
-  		
-   	for (j in result[i].resources) {
-  		urls.push(result[i].resources[j].url);
-  	} 
-}
-  console.log(urls.length)
+  result = snapshot.val();
+  console.log(result.length);
+
+   // for (var i in result){
+  	// 	result[i].tagscomma = result[i].tagscomma.split(", ");
+  	// };
+  	// console.log(result.length);
 });
+ 
